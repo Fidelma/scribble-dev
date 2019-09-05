73577350dffffd3b8c81a988f31cff1d8d3b0d41
evcka <filip.kaklin@gmail.com>
2019-09-05 22:24:55 +0100

    move name to postit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,8 @@
 <template lang="html">
   <div class="">
 
-  <h1>Scribble</h1>
   <img src="" alt="">
-  <div class="postit"></div>
+  <div class="postit"><h1>Scribble</h1></div>
   <button type="button" name="button" @click="startPlay()">PLAY</button>
 
   </div>
@@ -33,14 +32,15 @@ line-height: 1;
 text-align:center;     
 width: 275px;    
 margin: 45px;    
+margin-top: 25px;    
 min-height:200px;
 max-height:200px;
-padding-top:35px;
+padding-top: 25px;
 position:relative;   
 border:1px solid #E8E8E8;  
-border-top:60px solid #fdfd86;
-font-family:'Reenie Beanie';    
-font-size:22px;      
+border-top:30px solid #fdfd86;
+font-family:'Ubuntu';
+font-size:32px;      
 border-bottom-right-radius: 60px 5px;
 display:inline-block;    
  background: #ffff88; /* Old browsers */
