d8c8e80aa7f83901cde818bf24c07d88827278e9
evcka <filip.kaklin@gmail.com>
2019-09-05 23:27:18 +0100

    make ubuntu first font

--- src/App.vue
+++ src/App.vue
@@ -89,8 +89,7 @@ body {
 }
 
 #app {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+  font-family: "Ubuntu", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
 
   margin: 0;
