dfcd330d5419e9bdc85785f7e0d33ce5101ef594
evcka <filip.kaklin@gmail.com>
2019-09-12 22:23:51 +0100

    actually add the draw component

---
+++ src/components/Draw.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <h1>Draw!</h1>
+    <p>now put instruction here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'draw',
+  methods: {
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
